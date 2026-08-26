14f33a8ae020cfb6f1e7cdf9638abf1fda673f6d
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2026-08-26 11:09:36 +0800

    add log native

--- android/c/log.h
+++ android/c/log.h
@@ -2,6 +2,10 @@
 #define __LOG__
 #include <android/log.h>
 
+#ifndef _IDENTITY_
+#define _IDENTITY_ No_Defined
+#endif // _IDENTITY_
+
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,#_IDENTITY_,__VA_ARGS__)
 #define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN ,#_IDENTITY_,__VA_ARGS__)
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,#_IDENTITY_,__VA_ARGS__)
