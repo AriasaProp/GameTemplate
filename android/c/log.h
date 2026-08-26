ac4a82340e2d96019b88163262eeaaee757ae47c
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2026-08-26 11:38:01 +0800

    fix log

--- android/c/log.h
+++ android/c/log.h
@@ -1,14 +1,14 @@
 #ifndef __LOG__
 #define __LOG__
+
 #include <android/log.h>
 
-#ifndef _IDENTITY_
-#define _IDENTITY_ "No_Defined"
-#endif // _IDENTITY_
+#ifdef DEBUG
+#  define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, _IDENTITY_, __VA_ARGS__))
+#endif // DEBUG
 
-#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,_IDENTITY_,__VA_ARGS__)
-#define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN ,_IDENTITY_,__VA_ARGS__)
-#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,_IDENTITY_,__VA_ARGS__)
-#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO ,_IDENTITY_,__VA_ARGS__)
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO , _IDENTITY_, __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN , _IDENTITY_, __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, _IDENTITY_, __VA_ARGS__))
 
 #endif // __LOG__
