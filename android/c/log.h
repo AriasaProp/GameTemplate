fa2773f7f73ebfb4ac35db4c64421d1c55643a96
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2026-08-26 11:19:45 +0800

    add log id tag

--- android/c/log.h
+++ android/c/log.h
@@ -3,12 +3,12 @@
 #include <android/log.h>
 
 #ifndef _IDENTITY_
-#define _IDENTITY_ No_Defined
+#define _IDENTITY_ "No_Defined"
 #endif // _IDENTITY_
 
-#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,#_IDENTITY_,__VA_ARGS__)
-#define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN ,#_IDENTITY_,__VA_ARGS__)
-#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,#_IDENTITY_,__VA_ARGS__)
-#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO ,#_IDENTITY_,__VA_ARGS__)
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,_IDENTITY_,__VA_ARGS__)
+#define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN ,_IDENTITY_,__VA_ARGS__)
+#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,_IDENTITY_,__VA_ARGS__)
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO ,_IDENTITY_,__VA_ARGS__)
 
 #endif // __LOG__
